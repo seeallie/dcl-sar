46a4d9c21a69736ce86c454b27b6062dd468e42e
Allie Ai <allie.ai@dartmouth.edu>
2019-06-21 11:29:04 -0400

    Create a new empty branch

--- dcl_sar/js/dcl_sar.js
+++
@@ -1,7 +0,0 @@
-(function ($, Drupal) {
-  Drupal.behaviors.dclVboSar = {
-    attach: function (context, settings) {
-      $('#vbo-action-form-wrapper .form-item.form-item-action').hide(); 
-    }
-  };
-})(jQuery, Drupal);
